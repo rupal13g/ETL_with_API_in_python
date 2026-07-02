41f2983e6420ce671d5395c8fce111bbbd9c2370
rupal13g <guptarupaliit@gmail.com>
2026-07-02 13:02:47 +0530

    written code for extract.py file

---
+++ ETL/extract.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import requests
+
+BASE_URL = 'https://fakestoreapi.com/'
+
+def extract_products():
+    product_url = f'{BASE_URL}/products'
+    product_response = requests.get(product_url)
+    product_response.raise_for_status()
+
+    product = product_response.json()
+    product_dataframe = pd.DataFrame(product)
+
+    return product_dataframe
+
+def extract_users():
+    user_url = f'{BASE_URL}/users'
+    user_response = requests.get(user_url)
+    user_response.raise_for_status()
+
+    user = user_response.json()
+    user_dataframe = pd.DataFrame(user)
+
+    return user_dataframe
