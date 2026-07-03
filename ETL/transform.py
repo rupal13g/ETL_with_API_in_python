571ce2a5ebe44e593fe2b13342ba2331c72b052f
rupal13g <guptarupaliit@gmail.com>
2026-07-03 12:15:55 +0530

    Updated transform.py file

--- ETL/transform.py
+++ ETL/transform.py
@@ -20,3 +20,25 @@ def transform_product(product_dataframe):
 
     return product_df
 
+def transform_user(user_dataframe):
+    user_df = user_dataframe.copy()
+
+    user_df['first_name'] = user_df['name'].apply(lambda x: x['firstname'])
+    user_df['last_name'] = user_df['name'].apply(lambda x: x['lastname'])
+
+    user_df['street'] = user_df['address'].apply(lambda x: x['street'])
+    user_df['city'] = user_df['address'].apply(lambda x: x['city'])
+    user_df['zip_code'] = user_df['address'].apply(lambda x: x['zipcode'])
+
+    user_df = user_df.rename(columns={
+        'id'  : 'user_id',
+        'email' : 'user_email',
+        'username' : 'username',
+        'password' : 'user_password'
+    })
+
+    user_df = user_df[{
+        'user_id', 'username', 'first_name', 'last_name', 'user_email', 'user_password', 'street', 'city', 'zip_code'
+    }]
+
+    return user_df
