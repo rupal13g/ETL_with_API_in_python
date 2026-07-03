9b15dd5f6961a0b5226712227abc623fa2caaa9b
rupal13g <guptarupaliit@gmail.com>
2026-07-03 13:31:50 +0530

    Updated transform.py file

--- ETL/transform.py
+++ ETL/transform.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def transform_product(product_dataframe):
+def transform_products(product_dataframe):
     product_df = product_dataframe.copy()
 
     product_df = product_df.rename(columns={
@@ -12,15 +12,15 @@ def transform_product(product_dataframe):
         'image' : 'product_image'
     })
 
-    product_df = product_df[{
+    product_df = product_df[[
         'product_id', 'product_name', 'product_price', 'product_description', 'product_category', 'product_image'
-    }]
+    ]]
 
     product_df['product_price'] = product_df['product_price'].astype(float)
 
     return product_df
 
-def transform_user(user_dataframe):
+def transform_users(user_dataframe):
     user_df = user_dataframe.copy()
 
     user_df['first_name'] = user_df['name'].apply(lambda x: x['firstname'])
@@ -37,8 +37,37 @@ def transform_user(user_dataframe):
         'password' : 'user_password'
     })
 
-    user_df = user_df[{
+    user_df = user_df[[
         'user_id', 'username', 'first_name', 'last_name', 'user_email', 'user_password', 'street', 'city', 'zip_code'
-    }]
+    ]]
 
     return user_df
+
+def transform_carts(cart_dataframe):
+    carts_df = cart_dataframe.rename(columns={
+        "id": "cart_id",
+        "userId": "user_id"
+    })
+
+    carts_df["date"] = pd.to_datetime(carts_df["date"])
+
+    carts_df = carts_df[["cart_id", "user_id", "date"]]
+
+    return carts_df
+
+def transform_cart_items(cart_dataframe):
+    items = (
+        cart_dataframe.rename(columns={"id": "cart_id"})
+          [["cart_id", "products"]]
+          .explode("products")
+    )
+
+    products = pd.json_normalize(items["products"])
+
+    items = pd.concat(
+        [items.drop(columns="products").reset_index(drop=True),
+         products],
+        axis=1
+    )
+
+    return items.rename(columns={"productId": "product_id"})
