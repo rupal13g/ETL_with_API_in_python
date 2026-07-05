5fa6cbf6dadf11d376d6d771e57a11a842fa73ba
rupal13g <guptarupaliit@gmail.com>
2026-07-05 22:16:14 +0530

    Modified create_tables.sql file

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,35 +1,69 @@
-CREATE DATABASE FakeStoreDB;
+/*
+===========================================
+Project : Fake Store ETL
+Database: SQL Server
+Purpose : Create tables for ETL pipeline
+Author  : Rupal Gupta
+===========================================
+*/
+
+IF DB_ID('FakeStoreDB') IS NULL
+BEGIN
+    CREATE DATABASE FakeStoreDB;
+END;
+GO
 
 USE FakeStoreDB;
+GO
 
-CREATE TABLE users (
-    user_id INT NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    username VARCHAR(50) NOT NULL,
-    firstname VARCHAR(50) NOT NULL,
-    lastname VARCHAR(50) NOT NULL,
-    phone VARCHAR(30) NULL
-);
+IF OBJECT_ID('users', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.users (
+        user_id INT NOT NULL,
+        username VARCHAR(50) NOT NULL,
+        first_name VARCHAR(50) NOT NULL,
+        last_name VARCHAR(50) NOT NULL,
+        email VARCHAR(100) NOT NULL,
+        user_password VARCHAR(20) NOT NULL,
+        phone_number VARCHAR(20) NULL,
+        street VARCHAR(200) NOT NULL,
+        city VARCHAR(50) NOT NULL,
+        zip_code VARCHAR(10) NOT NULL
+    )
+END;
+GO
 
-CREATE TABLE products (
-    product_id INT NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    price DECIMAL(10,2) NOT NULL,
-    category VARCHAR(100) NOT NULL,
-    description VARCHAR(MAX) NULL,
-    image VARCHAR(MAX) NULL,
-    rating_rate FLOAT NOT NULL,
-    rating_count INT NOT NULL
-);
+IF OBJECT_ID('products', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.products (
+        product_id INT NOT NULL,
+        product_name VARCHAR(255) NOT NULL,
+        product_price DECIMAL(10,2) NOT NULL,
+        product_description VARCHAR(MAX) NULL,
+        product_category VARCHAR(100) NOT NULL,
+        product_image VARCHAR(MAX) NULL,
+        product_rating DECIMAL(3,2) NOT NULL,
+        product_rating_count INT NOT NULL
+    )
+END;
+GO
 
-CREATE TABLE carts (
-    cart_id INT NOT NULL,
-    user_id INT NOT NULL,
-    date DATE NOT NULL
-);
+IF OBJECT_ID('carts', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.carts (
+        cart_id INT NOT NULL,
+        user_id INT NOT NULL,
+        date DATE NOT NULL
+    )
+END;
+GO
 
-CREATE TABLE cart_items (
-    cart_id INT NOT NULL,
-    product_id INT NOT NULL,
-    quantity INT NOT NULL
-);
+IF OBJECT_ID('cart_items', 'U') IS NULL
+BEGIN
+    CREATE TABLE dbo.cart_items (
+        cart_id INT NOT NULL,
+        product_id INT NOT NULL,
+        quantity INT NOT NULL
+    )
+END
+GO
