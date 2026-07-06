857d6ecbabec655e296d7aa9cdf01a45834eccf9
rupal13g <guptarupaliit@gmail.com>
2026-07-06 18:36:03 +0530

    Modified exception handling and other code-parts in extract.py file

--- etl/extract.py
+++ etl/extract.py
@@ -2,52 +2,61 @@ import pandas as pd
 import requests
 import logging
 
-BASE_URL = 'https://fakestoreapi.com/'
+BASE_URL = 'https://fakestoreapi.com'
 
 def extract_products():
     product_url = f'{BASE_URL}/products'
-    # product_response = requests.get(product_url)
-    # product_response.raise_for_status()
     try:
         product_response = requests.get(product_url, timeout=30)
         product_response.raise_for_status()
     except requests.exceptions.RequestException as e:
-        logging.error(f"Failed to fetch data: {e}")
+        logging.error(
+            f"Failed to fetch products from {product_url}: {e}"
+        )
         raise
 
-    product = product_response.json()
-    product_dataframe = pd.DataFrame(product)
+    products = product_response.json()
+    products_dataframe = pd.DataFrame(products)
+    logging.info(
+        f"Extracted {len(products_dataframe)} products."
+    )
 
-    return product_dataframe
+    return products_dataframe
 
 def extract_users():
     user_url = f'{BASE_URL}/users'
-    # user_response = requests.get(user_url)
-    # user_response.raise_for_status()
     try:
         user_response = requests.get(user_url, timeout=30)
         user_response.raise_for_status()
     except requests.exceptions.RequestException as e:
-        logging.error(f"Failed to fetch data: {e}")
+        logging.error(
+            f"Failed to fetch products from {user_url}: {e}"
+        )
         raise
 
-    user = user_response.json()
-    user_dataframe = pd.DataFrame(user)
+    users = user_response.json()
+    users_dataframe = pd.DataFrame(users)
+    logging.info(
+        f"Extracted {len(users_dataframe)} products."
+    )
 
-    return user_dataframe
+    return users_dataframe
 
 def extract_carts():
     cart_url = f'{BASE_URL}/carts'
-    # cart_response = requests.get(cart_url)
-    # cart_response.raise_for_status()
     try:
         cart_response = requests.get(cart_url, timeout=30)
         cart_response.raise_for_status()
     except requests.exceptions.RequestException as e:
-        logging.error(f"Failed to fetch data: {e}")
+        logging.error(
+            f"Failed to fetch products from {cart_url}: {e}"
+        )
         raise
 
-    cart = cart_response.json()
-    cart_dataframe = pd.DataFrame(cart)
+    carts = cart_response.json()
+    carts_dataframe = pd.DataFrame(carts)
+    logging.info(
+        f"Extracted {len(carts_dataframe)} products."
+    )
 
-    return cart_dataframe
+    return carts_dataframe
